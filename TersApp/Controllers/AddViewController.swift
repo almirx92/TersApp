c866ab83aab2cda83489f5b96c1d5f9da42c7be4
Macbook <macbook@MacAG.local>
2021-07-20 18:09:12 +0200

    Styling of Add Form

--- TersApp/Controllers/AddViewController.swift
+++ TersApp/Controllers/AddViewController.swift
@@ -27,17 +27,41 @@ class AddViewController: FormViewController, MenuViewDelegate {
         menuView.delegate = self
         setup()
         form
-            +++ Section("JIB")
+            +++ Section(){ section in
+                var header = HeaderFooterView<UILabel>(.class)
+                header.height = {20.0}
+                header.onSetupView = {view, _ in
+                    view.textColor = UIColor(hexString: "#B5B5BE")
+                    view.text = "JIB"
+                    view.font = UIFont(name: "Roboto", size: 14)
+                }
+                section.header = header
+            }
             <<< TextRow(){ row in
                 row.placeholder = "4303425000001"
+                row.placeholderColor = .black
+                
+            }.cellSetup({cell, row in
+                cell.textLabel?.font = UIFont(name: "Roboto", size: 14)
                 
                 
+            })
+            +++ Section(){ section in
+                var header = HeaderFooterView<UILabel>(.class)
+                header.height = {20.0}
+                header.onSetupView = {view, _ in
+                    view.textColor = UIColor(hexString: "#B5B5BE")
+                    view.text = "PDV"
+                    view.font = UIFont(name: "Roboto", size: 14)
+                }
+                section.header = header
             }
-            +++ Section("PDV")
             <<< TextRow(){ row in
                 row.placeholder = "303425000001"
-            }
-      
+                row.placeholderColor = .black
+            }.cellSetup({cell, row in
+                cell.textLabel?.font = UIFont(name: "Roboto", size: 14)
+            })
             // Placeholder row for the border
             <<< TextRow() { row in
 
@@ -45,27 +69,76 @@ class AddViewController: FormViewController, MenuViewDelegate {
                 cell.height = {50.0}
                 cell.addBorder(for: .bottom, withColor: UIColor(hexString: "#F1F1F5"), borderWidth: 1.0)
                 cell.isUserInteractionEnabled = false
-                
-               
-                
             })
 
-            +++ Section("Email Address")
+            +++ Section(){ section in
+            var header = HeaderFooterView<UILabel>(.class)
+            header.height = {20.0}
+            header.onSetupView = {view, _ in
+                view.textColor = UIColor(hexString: "#B5B5BE")
+                view.text = "Email Address"
+                view.font = UIFont(name: "Roboto", size: 14)
+                }
+                section.header = header
+            }
+        
             <<< TextRow(){ row in
                 row.placeholder = "pearshadow@gmail.com"
-            }
-            +++ Section("Company Address")
+                row.placeholderColor = .black
+            }.cellSetup({cell, row in
+                cell.textLabel?.font = UIFont(name: "Roboto", size: 14)
+            })
+            +++ Section(){ section in
+                var header = HeaderFooterView<UILabel>(.class)
+                header.height = {20.0}
+                header.onSetupView = {view, _ in
+                    view.textColor = UIColor(hexString: "#B5B5BE")
+                    view.text = "Company Address"
+                    view.font = UIFont(name: "Roboto", size: 14)
+                    }
+                    section.header = header
+                }
             <<< TextRow(){ row in
                 row.placeholder = "24. Juna br. 15 71320, Vogosca, Bosna i Hercegovina"
-            }
-            +++ Section("Phone Number")
+                row.placeholderColor = .black
+            }.cellSetup({cell, row in
+                cell.textLabel?.font = UIFont(name: "Roboto", size: 14)
+            })
+            +++ Section(){ section in
+                var header = HeaderFooterView<UILabel>(.class)
+                header.height = {20.0}
+                header.onSetupView = {view, _ in
+                    view.textColor = UIColor(hexString: "#B5B5BE")
+                    view.text = "Phone Number"
+                    view.font = UIFont(name: "Roboto", size: 14)
+                    }
+                    section.header = header
+                }
             <<< TextRow(){ row in
                 row.placeholder = "+62 812 231 731 / +62 811 984 312"
-            }
-            +++ Section("Company Website")
+                row.placeholderColor = .black
+            }.cellSetup({cell, row in
+                cell.textLabel?.font = UIFont(name: "Roboto", size: 14)
+                cell.textField.textAlignment = .left
+            })
+            +++ Section(){ section in
+                var header = HeaderFooterView<UILabel>(.class)
+                header.height = {20.0}
+                header.onSetupView = {view, _ in
+                    view.textColor = UIColor(hexString: "#B5B5BE")
+                    view.text = "Company Website"
+                    view.font = UIFont(name: "Roboto", size: 14)
+                    }
+                    section.header = header
+                }
             <<< TextRow(){ row in
                 row.placeholder = "pearshadow.com"
-            }
+                row.placeholderColor = .black
+            }.cellSetup({cell, row in
+                cell.textLabel?.font = UIFont(name: "Roboto", size: 14)
+                
+                
+            })
             <<< TextRow() { row in
 
             }.cellSetup({ cell, row in
@@ -73,7 +146,25 @@ class AddViewController: FormViewController, MenuViewDelegate {
                 cell.addBorder(for: .bottom, withColor:UIColor(hexString: "#F1F1F5"), borderWidth: 1.0)
                 cell.isUserInteractionEnabled = false
             })
+            <<< TextRow() { row in
+
+            }.cellSetup({ cell, row in
+                cell.height = {50.0}
+                cell.isUserInteractionEnabled = false
+            })
+            <<< ButtonRow() {
+                   $0.title = "Add new user"
+                   $0.cell.backgroundColor = UIColor(hexString: "#F9EC1E")
+                $0.cell.tintColor = .black
+                $0.cell.layer.cornerRadius = 10.0
+                $0.cell.textLabel?.font = UIFont(name: "Roboto", size: 14)
+                           }
+                           .onCellSelection { cell, row in
+                               //send data to server function
+
+                   }
     }
+            
     override var prefersStatusBarHidden: Bool {
         return true
     }
